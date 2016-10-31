382fa026c114c3b12734c625c63af1c4ce1f6c81
Aman Agarwal <amanaamazing@gmail.com>
2016-10-30 21:43:25 -0700

    show movie page shows the movies average rating, user comments and a link to the add review page

--- Show_M.php
+++ Show_M.php
@@ -35,6 +35,7 @@ function actorsInMovie($identifier) {
             return "failed";
         }
         $queryRows = getAllRows($queryResult);
+        $queryResult->free();
         $actorsInMovie = array();
         foreach ($queryRows as $actorRole) {
             $actorsInMovie[$actorRole["aid"]] = array(
@@ -48,8 +49,41 @@ function actorsInMovie($identifier) {
     return array();
 }
 
-$movieInformation = movieInformation($_GET["identifier"]);
-$actorsInMovie = actorsInMovie($_GET["identifier"]);
+function averageScore($identifier) {
+    if ($identifier) {
+        $query =
+            "SELECT AVG(R.rating) FROM Movie M, Review R " .
+            "WHERE M.id = R.mid AND M.id = " . $identifier . ";";
+        $queryResult = runQuery($query);
+        if ($queryResult === "failed") {
+            return "failed";
+        }
+        $queryRows = getAllRows($queryResult);
+        $queryResult->free();
+        return $queryRows[0]["AVG(R.rating)"];
+    }
+    return array();
+}
+
+function getUserComments($identifier) {
+    if ($identifier) {
+        $query = "SELECT * FROM Review R WHERE R.mid = " . $identifier . ";";
+        $queryResult = runQuery($query);
+        if ($queryResult === "failed") {
+            return "failed";
+        }
+        $queryRows = getAllRows($queryResult);
+        $queryResult->free();
+        return $queryRows;
+    }
+    return array();
+}
+
+$identifier = $_GET["identifier"];
+$movieInformation = movieInformation($identifier);
+$actorsInMovie = actorsInMovie($identifier);
+$averageScore = averageScore($identifier);
+$userComments = getUserComments($identifier);
 ?>
 
 <html>
@@ -127,6 +161,43 @@ if ($actorsInMovie === "failed") {
     </table>
     <br><hr>
 <?php } ?>
+
+
+<?php
+if ($averageScore === "failed") {
+    print 'No Average Review';
+} else if (!empty($averageScore)) {
+    print '<h2>Average Score: </h2>';
+    print "The average score for this movie is: $averageScore";
+}
+?>
+
+<?php
+if ($userComments === "failed") {
+    print '<h2>Invalid Identifier</h2>';
+} else if (empty($userComments)) {
+    print '<h2>There are no user reviews for this movie</h2>';
+} else {
+    print '<h2>User Reviews: </h2>';
+    foreach ($userComments as $comment) {
+        $name = $comment["name"];
+        $rating = $comment["rating"];
+        $commentMessage = $comment["comment"];
+        $time = $comment["time"];
+        print "<p><b>$name</b> rated this movie $rating out of 5 and said: \"$commentMessage\" at $time.</p>";
+    }
+}
+?>
+
+<?php
+if ($movieInformation === "failed") {
+} else if (empty($movieInformation)) {
+} else {
+    print "<h3><a href='GiveReview.php?MovieID=$identifier'>Leave Your Own Review For This Movie</a></h3>";
+}
+?>
+
+
 
 <h2>Search Actors and Movies</h2>
 <form action="./search.php" method="GET">
