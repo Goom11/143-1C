466980688789a2b13f15805a158aa585605d7c06
Aman Agarwal <amanaamazing@gmail.com>
2016-10-30 19:23:10 -0700

    added an actor's role information to the show actor page

--- Show_A.php
+++ Show_A.php
@@ -1,5 +1,13 @@
 <?php
 
+function getAllRows($queryResult) {
+    $rows = [];
+    while ($row = $queryResult->fetch_assoc()) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
 // If you call this method
 //   make sure to free the result after you are done with it!!
 function runQuery($query) {
@@ -8,7 +16,6 @@ function runQuery($query) {
         die('Unable to connect to database [' . $db->connect_error . ']');
     }
 
-    $rv = "failed";
     if (!($rs = $db->query($query))) {
         $rv = "failed";
         $rs->free();
@@ -26,7 +33,11 @@ function actorInformation($identifier) {
         if ($queryResult === "failed") {
             return "failed";
         }
-        $queryRow = $queryResult->fetch_assoc();
+        $queryRows = getAllRows($queryResult);
+        if (count($queryRows) < 1) {
+            return array();
+        }
+        $queryRow = $queryRows[0];
         $actorInformation = array(
             "name" => $queryRow["first"] . " " . $queryRow["last"],
             "sex" => $queryRow["sex"],
@@ -36,22 +47,50 @@ function actorInformation($identifier) {
         $queryResult->free();
         return $actorInformation;
     }
+    return array();
+}
+
+function movieRoleInformation($identifier) {
+    if ($identifier) {
+        $query =
+            "SELECT * FROM MovieActor MA, Movie M WHERE MA.mid = M.id AND" .
+            " MA.aid = " . $identifier . ";";
+        $queryResult = runQuery($query);
+        if ($queryResult === "failed") {
+            return "failed";
+        }
+        $queryRows = getAllRows($queryResult);
+        $queryResult->free();
+
+        $movieRoleInformation = array_map(function ($row) {
+            return array(
+                "role" => $row["role"],
+                "title" => $row["title"],
+            );
+        }, $queryRows);
+        return $movieRoleInformation;
+    }
+    return array();
 }
 
 $actorInformation = actorInformation($_GET["identifier"]);
+
+$movieRoleInformation = movieRoleInformation($_GET["identifier"]);
+
 ?>
 
 <html>
 <head>
-    <?php $title = "IMDB: I(ncomplete and Dated) Movie DB" ?>
-    <title><?php print "$title"; ?></title>
+<?php $title = "IMDB: I(ncomplete and Dated) Movie DB" ?>
+
+<title><?php print "$title"; ?></title>
 </head>
 
 <body bgcolor=white>
 <h1><?php print "$title"; ?></h1>
 
 <?php if (!empty($actorInformation)) { ?>
-    <h2>Actor Information</h2>
+<h2>Actor Information:</h2>
 <table border="1" cellpadding="2" cellspacing="1">
     <thead>
     <tr>
@@ -74,27 +113,39 @@ $actorInformation = actorInformation($_GET["identifier"]);
     </tr>
     </tbody>
 </table>
-<br><hr>
 <?php } ?>
 
-<?php
+<?php if (!empty($movieRoleInformation)) { ?>
+<h2>Actor's Movies and Role:</h2>
+<table border="1" cellpadding="2" cellspacing="1">
+    <thead>
+    <tr>
+        <td>Role</td>
+        <td>Movie Title</td>
+    </tr>
+    </thead>
 
-/*
-    <td>Tom Hanks</td>
-    <td>Male</td>
-    <td>1956-07-09</td>
-    <td>Still Alive</td>
-*/
-
-$identifier = $_GET["identifier"];
-?>
+    <tbody>
+    <?php
+    foreach($movieRoleInformation as $row) {
+        print '<tr>';
+        foreach ($row as $info) {
+            print '<td>';
+            print $info;
+            print '</td>';
+        }
+        print '</tr>';
+    }
+    ?>
+    </tbody>
+</table>
+<?php } ?>
 
 <h2>Search Actors and Movies</h2>
 <form action="./search.php" method="GET">
     <input type="text" name="query" size="40" <br>
     <input type="submit" value="Submit">
 </form>
-
 
 </body>
 </html>
